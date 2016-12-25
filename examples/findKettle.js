1551a5550c85811ebec06bebacf996a103f2bea0
Timan Rebel <timan@trebel.nl>
2016-12-25 15:55:27 +0100

    Made minor changes to make code work

--- examples/findKettle.js
+++ examples/findKettle.js
@@ -1,6 +1,6 @@
 var iKettleFinder = require('../index').iKettleFinder;
 
-iKettleFinder.discoverKettles().then(function(kettles) {
+iKettleFinder.discoverKettles('10.0.1.0/24').then(function(kettles) {
   if (kettles.length > 0) {
     var kettle = kettles[0];
     kettle.connect().then(function() {
